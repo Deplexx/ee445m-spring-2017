a1326d27c8ebed3e58ddf3d3918b775374c2892c
Nico Cortes <nico.crts@gmail.com>
2017-02-07 01:13:49 -0600

    Updated the lcd code to use the ST7735_Message routine

--- ValvanoWareTM4C123/lab1/uart_interp.c
+++ ValvanoWareTM4C123/lab1/uart_interp.c
@@ -185,14 +185,38 @@ static void lcd_runComm(const char *comm) {
     if(strcmp((const char*) currTok, "on") == 0)
         ST7735_InitR(INITR_REDTAB);
     else if(strcmp((const char*) currTok, "echo") == 0) {
-        char* str = strtok(NULL, " \t");
+        int device = nextInt();
+        int line = nextInt();
+        char* string = strtok(NULL, " \t");
+        uint32_t value = nextInt();
 
-        if(str == NULL) {
-            UART_OutStringCRLF("Invalid message; enter a valid one.");
+        if(device == NULL)
+            device = 0;
+        else if(device > 1 || device < 0) {
+            UART_OutStringCRLF("Invalid device; enter a number in {0,1}");
             return;
         }
 
-        ST7735_OutString(str);
-    } else
+        if(line == NULL)
+            line = 0;
+        else if(line > 3 || line < 0) {
+            UART_OutStringCRLF("Invalid line; enter a number <= 3 && >= 1");
+            return;
+        }
+
+        if(string == NULL) {
+            UART_OutStringCRLF("You must provide a message.");
+            return;
+        }
+
+        if(value == NULL) {
+            UART_OutStringCRLF("You must provide a value.");
+            return;
+        }
+
+        ST7735_Message(device, line, string, value);
+    } else if(strcmp(currTok, "clear") == 0)
+      Output_Clear();
+    else
       UART_OutStringCRLF("Invalid lcd command. Type \"lcd -h\" for a list of commands.");
 }
